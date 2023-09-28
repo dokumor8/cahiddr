3112b600b01e8fd698ffb6d0e5375f2e83083335
dokumor8 <146167157+dokumor8@users.noreply.github.com>
2023-09-28 22:34:22 +0300

    Add building tile highlight mode

--- node_2d.gd
+++ node_2d.gd
@@ -3,39 +3,52 @@ extends Node2D
 @onready var walk_marker = $WalkMarker
 @onready var hero = $Hero
 @onready var camera = $Camera2D
-var cursor_state = "move"
+@onready var tile_map = $TileMap
+@onready var tile_highlighter = $TileHighlighter
 
+var cursor_state = "move"
 var camera_speed = 500
+var cursor_mode = "normal"
 
 
 func _input(event):
 	if event.is_action_pressed("right_click"):
-#		Array[Dictionary]
-#		var parameters
-		var space = get_world_2d().direct_space_state
-		var parameters = PhysicsPointQueryParameters2D.new()
-		parameters.position = get_global_mouse_position()
-		parameters.collision_mask = 0x00000002
-#		print(parameters.position)
 
-#		var physics = PhysicsDirectSpaceState2D.new()
-#		parameters.
-		var intersect_objects = space.intersect_point(parameters, 4)
-#		print(intersect_objects)
-		if (intersect_objects.is_empty()):
-			walk_marker.global_position = get_global_mouse_position()
-			walk_marker.show()
-			hero.walk_to(walk_marker)
-		else:
-			var enemy_collider = intersect_objects[0]["collider"]
-			hero.set_attack_target(enemy_collider)
-			walk_marker.hide()
-			# TODO attack
-#
-#		if cursor_state == "move":
-#
-#		elif cursor_state == "attack":
-#			print("attack")
+		if cursor_mode == "normal":
+	#		Array[Dictionary]
+	#		var parameters
+			var space = get_world_2d().direct_space_state
+			var parameters = PhysicsPointQueryParameters2D.new()
+			parameters.position = get_global_mouse_position()
+			parameters.collision_mask = 0x00000002
+	#		print(parameters.position)
+
+	#		var physics = PhysicsDirectSpaceState2D.new()
+	#		parameters.
+			var intersect_objects = space.intersect_point(parameters, 4)
+	#		print(intersect_objects)
+			if (intersect_objects.is_empty()):
+				walk_marker.global_position = get_global_mouse_position()
+				walk_marker.show()
+				hero.walk_to(walk_marker)
+			else:
+				var enemy_collider = intersect_objects[0]["collider"]
+				hero.set_attack_target(enemy_collider)
+				walk_marker.hide()
+				# TODO attack
+		
+		elif cursor_mode == "build":
+			cursor_mode = "normal"
+			tile_highlighter.hide()
+
+	if event.is_action_pressed("left_click"):
+		if cursor_mode == "normal":
+			cursor_mode = "build"
+			start_build_mode()
+		
+		elif cursor_mode == "build":
+			pass
+			# try building here - refuse or build
 
 
 #var walk_marker = $WalkMarker
@@ -43,15 +56,28 @@ func _input(event):
 #func _input(event):
 #	if event.is_action_pressed("right_click"):
 #		walk_marker.position = get_global_mouse_position()
-#
-#
+
+func start_build_mode():
+	pass
+
 func _physics_process(delta):
 	
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	var camera_velocity = input_direction * camera_speed
 	camera.translate(delta * camera_velocity)
 
-#
+
+	if cursor_mode == "build":
+		var mouse_coords = get_global_mouse_position()
+		var tile_coords = tile_map.local_to_map(mouse_coords)
+		tile_coords.x -= tile_coords.x % 2
+		tile_coords.y -= tile_coords.y % 2
+		var snapped_local_coords = tile_map.map_to_local(tile_coords)
+		print(snapped_local_coords)
+		tile_highlighter.position = snapped_local_coords
+		tile_highlighter.show()
+
+
 #func _process(delta):
 #	if Input.is_action_pressed("right_click"):
 #		walk_marker.show()
@@ -63,7 +89,7 @@ func _physics_process(delta):
 func _on_hero_end_movement():
 	walk_marker.hide()
 
-#
+
 #func _on_unit_shoot_target(Bullet, direction, location):
 #	var spawned_bullet = Bullet.instantiate()
 #	add_child(spawned_bullet)
