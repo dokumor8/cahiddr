b07f871ff9c030465efb942e61111be0acd7fded
dokumor8 <146167157+dokumor8@users.noreply.github.com>
2023-11-18 23:04:30 +0300

    Add enemy camp logic start

--- src/enemy_structures/small_camp.gd
+++ src/enemy_structures/small_camp.gd
@@ -5,24 +5,16 @@ var spawn_height = 40
 @export var spawn_area :RectangleShape2D
 var rng = RandomNumberGenerator.new()
 var Unit = preload("res://src/units/unit.tscn")
+var camp_level = 2
+@onready var state_chart = $StateChart
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	seed(1)
 	
-	await get_tree().create_timer(1, false).timeout
-	await spawn_sequence(1, 1)
-	await get_tree().create_timer(5, false).timeout
-	await spawn_sequence(4, 0.1)
-	await get_tree().create_timer(15, false).timeout
-	await spawn_sequence(8, 0.1)
-	await get_tree().create_timer(15, false).timeout
-	await spawn_sequence(16, 0.3)
-	await get_tree().create_timer(30, false).timeout
-	await spawn_sequence(100, 1)
 
 
-func spawn_sequence(amount, delay):
+func spawn_wave(amount, delay):
 	spawn_enemy()
 	for i in amount - 1:
 		await get_tree().create_timer(delay, false).timeout
@@ -41,5 +33,26 @@ func spawn_enemy():
 	unit.global_position = global_position + Vector2(x_spawn, y_spawn)
 
 
-func _on_king_died():
-	$Timer.stop()
+
+func _on_send_wave_state_entered():
+	print("spawning unit")
+	await spawn_wave(camp_level, 0.1)
+	state_chart.send_event("building_finished")
+
+
+func _on_upgrade_state_entered():
+	camp_level += 1
+	print("camp level up", camp_level)
+	state_chart.send_event("upgrading_finished")
+
+
+func _on_building_units_state_entered():
+	print("started_building")
+	state_chart.send_event("building_started")
+	pass # Replace with function body.
+
+
+func _on_upgrading_state_entered():
+	print("upgrading_started")
+	state_chart.send_event("upgrading_started")
+	pass # Replace with function body.
