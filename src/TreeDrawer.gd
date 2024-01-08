ebfc3bdbf2f343d64d2c41dab6bb988258c33c9d
dokumor8 <146167157+dokumor8@users.noreply.github.com>
2024-01-08 12:43:51 +0300

    Add new tiles

--- src/TreeDrawer.gd
+++ src/TreeDrawer.gd
@@ -1,19 +1,28 @@
-#@tool
+@tool
 extends Node2D
 @export var tilemap: TileMap
 @export var tree_texture: Texture2D:
 	set = _set_texture
-#var TreeSprite = preload("res://src/units/tree_sprite.tscn")
+
+
+var TreeSprite = preload("res://src/units/tree_sprite.tscn")
 
 # Called when the node enters the scene tree for the first time.
 var tile_array = []
+var rng
 #var tree_texture
 
 func _ready():
+	rng = RandomNumberGenerator.new()
+	seed(1)
 	print("hint")
 	#tree_texture = ImageTexture.create_from_image(tree_image)
+	#await get_tree().create_timer(0.1, false).timeout
+	const tile_shift = Vector2(-16, -16)
 	
-	var tiles = tilemap.get_used_cells_by_id(-1, 2, Vector2i(1, 13), 0)
+	const shift_magnitude = 10
+	var tiles = tilemap.get_used_cells_by_id(0, 2, Vector2i(1, 13), 0)
+	print(tiles)
 	for tile in tiles:
 		print("ready")
 		print(tile)
@@ -21,12 +30,14 @@ func _ready():
 		print(snapped_local_coords)
 		snapped_local_coords += Vector2(tilemap.tile_set.tile_size)/2
 		print(snapped_local_coords)
-		tile_array.append(snapped_local_coords)
-		#var tree = TreeSprite.instantiate()
-		#tree.global_position = snapped_local_coords
-		#add_child(tree)
-	await get_tree().create_timer(0.5, false).timeout
-	queue_redraw()
+		var shift_x = rng.randi_range(-shift_magnitude, shift_magnitude)
+		var shift_y = rng.randi_range(-shift_magnitude, shift_magnitude)
+		var shift_vector = Vector2(shift_x, shift_y)
+		tile_array.append(snapped_local_coords + shift_vector)
+
+		var tree = TreeSprite.instantiate()
+		tree.global_position = snapped_local_coords + tile_shift + shift_vector
+		add_child(tree)
 
 
 func _set_texture(value):
@@ -41,11 +52,15 @@ func _process(delta):
 
 
 func _draw():
-	if true:
-	#for tile in tile_array:
-		var tile = Vector2(1000, 1000)
-		print("drawing")
-		print(tile)
-		print(tree_texture)
-		draw_rect(Rect2(tile.x, tile.y, 32, 32), Color.SKY_BLUE, true)
-		draw_texture(tree_texture, tile)
+	pass
+	#if Engine.is_editor_hint():
+#
+		##if true:
+		#const tile_shift = Vector2(-80, -160)
+		#for tile in tile_array:
+			##var shift_x = rng.randi_range(-shift_magnitude, shift_magnitude)
+			##var shift_y = rng.randi_range(-shift_magnitude, shift_magnitude)
+			##var shift_vector = Vector2(shift_x, shift_y)
+			##var tile = Vector2(1000, 1000)
+			##draw_rect(Rect2(tile.x, tile.y, 32, 32), Color.SKY_BLUE, true)
+			#draw_texture(tree_texture, tile + tile_shift)
